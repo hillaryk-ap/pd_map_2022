c30219e00c72d30e1285d419a1d6a6e3adda465d
hillaryk-ap <hkhan@catalystcalifornia.org>
2022-09-24 12:44:42 -0700

    2nd session changes

--- src/index.js
+++ src/index.js
@@ -12,21 +12,56 @@ let map;
 // Reference: https://docs.mapbox.com/mapbox-gl-js/api/map/
 
 async function init() {
+    // This is where we load the site data
+    //const sites = await import("../data/sites.json"); // remove because we don't need to store the excess data after geospatial processing (see process.mjs)
+        // also means we remove sites from custom-style.json:
+        // "sites" : {
+        //    "type": "geojson",
+        //    "data": {
+        //        "type": "FeatureCollection",
+        //        "features": []
+        //    }
+        //},
+    const neighborhoods = await import("../data/output.json"); // switched out from neighborhoods.json after apply geospatial processing (see process.mjs)
     const style = map.getStyle();
 
+    // These sources and layer styles are defined in the custom-style.json file
+    // Sources are data pulled in the map: for this course we are using geojson data is from historicplacesla.org
+        // MapBox geojson source documentation: https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/#geojson
+        // We add an empty features array for the site.geojson source that will get populated later with JS
+    // Layers determine how sources are visualized
     style.sources = {
         ...style.sources,
         ...custom.sources
     };
+
+    // Layers doc: https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/
+    // For point data we'll use circle layer type: https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#circle
+    // All layers have layout properties and paint properties
+        // layout properties should all be grouped together (same for paint)
     style.layers.push(...custom.layers);
     map.setStyle(style);
+
+    // Add data to the source: https://docs.mapbox.com/mapbox-gl-js/api/map/#map#getsource see getSource()
+        // geojson source doc: https://docs.mapbox.com/mapbox-gl-js/api/sources/#geojsonsource see setData()
+    //map.getSource("sites").setData(sites); // remove because we don't need to store the excess data after geospatial processing (see process.mjs)
+    map.getSource("neighborhoods").setData(neighborhoods);
+
 }
 
 // Mapbox requires an access token
 mapboxgl.accessToken = settings.accessToken;
-
 // Creates the map instance
 map = new mapboxgl.Map(settings);
-
 // Adds an event listener that calls init (callback function) when map "loads" 
 map.on("load", init);
+
+
+
+
+
+// Other notes
+// mapbox expressions doc: https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions/ - for custom-syle.json
+    // create color ramp with step function
+    // nest a get expression to get the count 
+    // we'll use stop values (have an output) to apply the color
--- src/process.mjs
+++ src/process.mjs
@@ -1,1 +1,45 @@
-console.log("success. 👍");
+// Where we do geospatial processing of geojson data with turf
+// Turf docs: https://turfjs.org/
+
+import turf from "@turf/turf"; // was added as a dependency in packages.json
+import fs from "fs"; // included with nodeJS
+import sites from "../data/sites.json" assert { type: "json" };
+import neighborhoods from "../data/neighborhoods.json" assert { type: "json" };
+
+
+
+// turf aggregation method - collect: https://turfjs.org/docs/#collect
+    // collect requires a "property"
+    // our data does not have any properties so we need to do some data manipulation first with js (not turf)
+    // we'll loop through each feature of our feature collection and add the property attribute with a count of 1 (each feature represents a site)
+    // then we'll be sum this counts to some polygon space (like a neighborhood)
+sites.features.forEach(function(feature) {
+    feature.properties = {
+        count: 1
+    };
+
+});
+
+
+let output = turf.collect(neighborhoods, sites, "count", "count")
+
+console.log(output.features[0]) // We can see here that the counts are literally aggregated into an array (i.e. it doesn't sum the counts)
+// However the length of the array is our sum -- so we can do a little extra data manipulation on the output argument of collect 
+
+output.features = output.features.filter(function(feature, index) {
+    feature.id = index; //the neighborhoods data was missing a unique identifier so here we assign
+    feature.properties.count = feature.properties.count.length; // this updates the count to the sum via the length (see above)
+    return feature.properties.count > 0; // this is required for the filter function 
+});
+
+console.log(output.features[0]) //looks good!
+
+// Now we need to write our output to a file
+    // First we need to convert the output from an object to a string
+output = JSON.stringify(output, null, "\t")
+
+    // Then we can write it out to a file
+fs.writeFile("../data/output.json", output, function(error) {
+    if (error) throw error;
+    console.log("file written with success. 👍");
+});
